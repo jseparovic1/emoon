f9f93835ca8632fb2b2f35cbc65fb7082e076710
Jurica Šeparović <jsepar00@fesb.hr>
2018-01-06 14:53:40 +0100

    add show exchange inital page

--- src/Controller/ExchangeController.php
+++ src/Controller/ExchangeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Exchange;
 use App\Repository\ExchangeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,5 +23,18 @@ class ExchangeController extends Controller
     {
         $exchanges = $this->exchangeRepository->findAll();
         return $this->render('exchange/index.html.twig', compact('exchanges', $exchanges));
+    }
+
+    public function show(string $id): Response
+    {
+        $exchange = $this->exchangeRepository->findOneBy(['id' => $id]);
+
+        if (!$exchange instanceof Exchange) {
+            $this->createNotFoundException('Exchange not found!');
+        }
+
+        return $this->render('exchange/show.html.twig', [
+            'exchange' => $exchange
+        ]);
     }
 }
